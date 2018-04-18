0fdca4ed0f166207c267516a404f191cc18ad524
longyf <longyf@pku.edu.cn>
2018-04-18 15:45:08 +0800

    Add (base==0&&exponent<0)
    特殊情况：底数为0，指数为负。我的处理是返回异常。
    特殊情况：0的0次方，按照c++中Power()的写法，返回1。
    目前的程序写法不好，没有把正数个double相乘的部分单独写出来，并且这一部分效率也不高。

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ int main() {
 	cout<<Power(2,3)<<endl;
 	cout<<Power(2,0)<<endl;
 	cout<<Power(4,-2)<<endl;
-	cout<<Power(0,-2)<<endl;
+	try {Power(0,-2);}
+	catch (invalid_argument e) {
+		cout<<e.what()<<endl;
+	}
 	return 0;
 }
--- myPower.h
+++ myPower.h
@@ -1,6 +1,7 @@
 #ifndef my_power_h
 #define my_power_h
 #include <iostream>
+#include <stdexcept>
 using namespace std;
 double Power(double base, int exponent) {
 	double mybase=base;
@@ -12,6 +13,9 @@ double Power(double base, int exponent) {
 		}
 	}
 	else {
+		if (base==0) 
+			throw invalid_argument("The result is infinite.");
+
 		exponent=-exponent;
         while (exponent!=1) {
             base*=mybase;
